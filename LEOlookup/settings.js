f602f3cb4d0bac48f72f3d10772259b1eeb46321
Ramiro Gómez <web@ramiro.org>
2013-03-01 20:02:55 +0100

    adjusted URLs to new UI and added Portuguese and Polish options

--- LEOlookup/settings.js
+++ LEOlookup/settings.js
@@ -5,37 +5,49 @@ var sites = [
     {
         'id': 'ende.dict.leo.org',
         'name': 'Englisch <-> Deutsch',
-        'url': 'http://dict.leo.org/ende?lp=ende&search=#SEARCH#',
+        'url': 'http://dict.leo.org/ende/index_de.html#/search=#SEARCH#',
         'locale': 'uk'
     },
     {
         'id': 'frde.dict.leo.org',
         'name': unescape('Franz%F6sisch') + ' <-> Deutsch',
-        'url': 'http://dict.leo.org/frde?lp=frde&search=#SEARCH#',
+        'url': 'http://dict.leo.org/frde/index_de.html#/search=#SEARCH#',
         'locale': 'fr'
     },
     {
         'id': 'esde.dict.leo.org',
         'name': 'Spanisch <-> Deutsch',
-        'url': 'http://dict.leo.org/esde?lp=esde&search=#SEARCH#',
+        'url': 'http://dict.leo.org/esde/index_de.html#/search=#SEARCH#',
         'locale': 'es'
     },
     {
         'id': 'itde.dict.leo.org',
         'name': 'Italienisch <-> Deutsch',
-        'url': 'http://dict.leo.org/itde?lp=itde&search=#SEARCH#',
+        'url': 'http://dict.leo.org/itde/index_de.html#/search=#SEARCH#',
         'locale': 'it'
     },
     {
         'id': 'chde.dict.leo.org',
         'name': 'Chinesisch <-> Deutsch',
-        'url': 'http://dict.leo.org/chde?lp=chde&search=#SEARCH#',
+        'url': 'http://dict.leo.org/chde/index_de.html#/search=#SEARCH#',
         'locale': 'ch'
     },
     {
         'id': 'rude.dict.leo.org',
         'name': 'Russisch <-> Deutsch',
-        'url': 'http://dict.leo.org/rude?lp=rude&search=#SEARCH#',
+        'url': 'http://dict.leo.org/rude/index_de.html#/search=#SEARCH#',
         'locale': 'ru'
+    },
+    {
+        'id': 'ptde.dict.leo.org',
+        'name': 'Portugiesisch <-> Deutsch',
+        'url': 'http://dict.leo.org/ptde/index_de.html#/search=#SEARCH#',
+        'locale': 'pt'
+    },
+    {
+        'id': 'plde.dict.leo.org',
+        'name': 'Polnisch <-> Deutsch',
+        'url': 'http://dict.leo.org/plde/index_de.html#/search=#SEARCH#',
+        'locale': 'pl'
     }
 ];
--- README.markdown
+++ README.markdown
@@ -1,7 +1,6 @@
 #LEO Wörterbuchsuche
 
-LEO Wörterbuchsuche is an extension for Google's Chrome browser to facilitate looking up words in the context of Web pages using the [dict.leo.org](http://dict.leo.org/) dictionary service. LEO searches their databases of translations to and from German paired with one of the following languages: English, French, Spanish, Italian, Chinese, and Russian.
+LEO Wörterbuchsuche is an extension for Google's Chrome browser to facilitate looking up words in the context of Web pages using the [dict.leo.org](http://dict.leo.org/) dictionary service. LEO searches their databases of translations to and from German paired with one of the following languages: English, French, Spanish, Italian, Chinese, Russian, Portuguese, and Polish.
 
 ##Links
 [Extension page in Chrome Web Store](https://chrome.google.com/webstore/detail/ojniiiidjmoaiehegaedmfdclmgmmpdp)
-
