ad4e55e6bd3408c2bdd26d51e5500c899aff82f2
Ramiro Gómez <code@ramiro.org>
2018-01-07 21:01:19 +0100

    Adjust chrome extension to dict.leo.org URL changes Make sure extension icon is colored in the extension toolbar

--- LEOlookup/settings.js
+++ LEOlookup/settings.js
@@ -5,49 +5,49 @@ var sites = [
     {
         'id': 'ende.dict.leo.org',
         'name': 'Englisch <-> Deutsch',
-        'url': 'http://dict.leo.org/ende/index_de.html#/search=#SEARCH#',
+        'url': 'https://dict.leo.org/englisch-deutsch/#SEARCH#',
         'locale': 'uk'
     },
     {
         'id': 'frde.dict.leo.org',
         'name': unescape('Franz%F6sisch') + ' <-> Deutsch',
-        'url': 'http://dict.leo.org/frde/index_de.html#/search=#SEARCH#',
+        'url': 'https://dict.leo.org/französisch-deutsch/#SEARCH#',
         'locale': 'fr'
     },
     {
         'id': 'esde.dict.leo.org',
         'name': 'Spanisch <-> Deutsch',
-        'url': 'http://dict.leo.org/esde/index_de.html#/search=#SEARCH#',
+        'url': 'https://dict.leo.org/spanisch-deutsch/#SEARCH#',
         'locale': 'es'
     },
     {
         'id': 'itde.dict.leo.org',
         'name': 'Italienisch <-> Deutsch',
-        'url': 'http://dict.leo.org/itde/index_de.html#/search=#SEARCH#',
+        'url': 'https://dict.leo.org/italienisch-deutsch/#SEARCH#',
         'locale': 'it'
     },
     {
         'id': 'chde.dict.leo.org',
         'name': 'Chinesisch <-> Deutsch',
-        'url': 'http://dict.leo.org/chde/index_de.html#/search=#SEARCH#',
+        'url': 'https://dict.leo.org/chinesisch-deutsch/#SEARCH#',
         'locale': 'ch'
     },
     {
         'id': 'rude.dict.leo.org',
         'name': 'Russisch <-> Deutsch',
-        'url': 'http://dict.leo.org/rude/index_de.html#/search=#SEARCH#',
+        'url': 'https://dict.leo.org/russisch-deutsch/#SEARCH#',
         'locale': 'ru'
     },
     {
         'id': 'ptde.dict.leo.org',
         'name': 'Portugiesisch <-> Deutsch',
-        'url': 'http://dict.leo.org/ptde/index_de.html#/search=#SEARCH#',
+        'url': 'https://dict.leo.org/portugiesisch-deutsch/#SEARCH#',
         'locale': 'pt'
     },
     {
         'id': 'plde.dict.leo.org',
         'name': 'Polnisch <-> Deutsch',
-        'url': 'http://dict.leo.org/plde/index_de.html#/search=#SEARCH#',
+        'url': 'https://dict.leo.org/polnisch-deutsch/#SEARCH#',
         'locale': 'pl'
     }
 ];
---
+++ leo-cxt-description.md
@@ -0,0 +1,7 @@
+Mit dieser Chrome-Extension kann einfach aus dem Kontext der aktuell geöffneten Seite heraus nach Übersetzungen für die Sprachpaare Deutsch - Englisch, Deutsch - Französisch, Deutsch - Spanisch, Deutsch - Italienisch, Deutsch - Chinesisch, Deutsch - Russisch, Deutsch - Portugiesisch und Deutsch - Polnisch in den Online-Wörterbüchern von LEO gesucht werden.
+
+Funktionsweise
+Markiert ein Benutzer mit der Maus ein Wort und klickt auf die rechte Maustaste wird im Kontextmenü ein Menüpunkt "LEO Wörterbuchsuche" und darunter die verfügbaren Sprachen angezeigt.
+
+Wählt ein Benutzer in den Einstellungen (Options) der Extension nur eine Sprache aus, wird der Link zur entsprechenden Wortübersetzung direkt im Kontextmenü angezeigt.
+Klickt der Benutzer auf eine Sprache wird einer neuer Browser-Tab geöffnet und der Benutzer gelangt auf die entsprechende LEO-Suchseite.
